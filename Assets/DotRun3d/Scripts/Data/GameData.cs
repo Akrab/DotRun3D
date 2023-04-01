8da052311c315d7e75b87eaea61da2480654cde0
akrab <Akrab@users.noreply.github.com>
2023-04-02 02:31:21 +0500

    Added GameData

---
+++ Assets/DotRun3d/Scripts/Data/GameData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DonRun3D.Data
+{
+    public class GameData 
+    {
+
+
+
+        public enum State
+        {
+            None,
+            Menu,
+            Game,
+
+        }
+    }
+}
