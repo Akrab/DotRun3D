b9c1d9433f920d428ca3c65d4be74c85e5ec4333
akrab <Akrab@users.noreply.github.com>
2023-04-02 02:31:08 +0500

    Added ColumnView

---
+++ Assets/DotRun3d/Scripts/World/ColumnView.cs
@@ -0,0 +1,11 @@
+using Akrab;
+using UnityEngine;
+
+namespace DonRun3D.World.Column
+{
+    public class ColumnView : CustomBehaviour
+    {
+        [SerializeField] private Transform _playerTarget;
+        [SerializeField] private Transform _bonusTarget;
+    }
+}
